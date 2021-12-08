5210637ac37302285be0eb47b61bb56f0fbad44e
m-r-ebrahimi <emohammadreza568@gmail.com>
2021-12-08 12:20:38 +0330

    complete menus.

--- src/main/java/menu/PlaySoccerMenu.java
+++ src/main/java/menu/PlaySoccerMenu.java
@@ -99,7 +99,66 @@ public class PlaySoccerMenu extends Menu implements CrudMenu {
 
     @Override
     public void update() throws IllegalAccessException {
+        System.out.println("Enter match id:");
+        int id=scanner.nextInt();
+        scanner.nextLine();
+        PlaySoccer playSoccer = new PlaySoccerService().loadById(id);
+        Team teamHome = playSoccer.getTeamHome();
+        int teamHomePoints = playSoccer.getTeamHomePoints();
+        Team teamAway = playSoccer.getTeamAway();
+        int teamAwayPoints = playSoccer.getTeamAwayPoints();
+        teamHome.setScore(teamHome.getScore() - teamHomePoints);
+        teamAway.setScore(teamAway.getScore() - teamAwayPoints);
+        new TeamService().saveOrUpdate(teamHome);
+        new TeamService().saveOrUpdate(teamAway);
 
+
+
+
+        System.out.println("Enter match date:");
+        Date date = new Date(scanner.nextLine());
+        System.out.println("Enter stadium id:");
+        Stadium stadium = new StadiumService().loadById(scanner.nextInt());
+        System.out.println("Enter team (home) id:");
+        teamHome = new TeamService().loadById(scanner.nextInt());
+        System.out.println("Enter team (away) id:");
+        teamAway = new TeamService().loadById(scanner.nextInt());
+        System.out.println("Enter ball id:");
+        Ball ball = new BallService().loadById(scanner.nextInt());
+        scanner.nextLine();
+        System.out.println("Enter team home - away goals:");
+        String goals = scanner.nextLine();
+        String[] goal = goals.split(" - ");
+        int teamHomeGoal = Integer.parseInt(goal[0]);
+        int teamAwayGoal = Integer.parseInt(goal[1]);
+        int teamHomePoint;
+        int teamAwayPoint;
+
+        if (teamHomeGoal - teamAwayGoal > 0) {
+            teamHomePoint = 3;
+            teamAwayPoint = 0;
+        } else if (teamHomeGoal - teamAwayGoal < 0) {
+            teamHomePoint = 0;
+            teamAwayPoint = 3;
+        } else {
+            teamHomePoint = 1;
+            teamAwayPoint = 1;
+        }
+
+        playSoccer.setMatchDate(date);
+        playSoccer.setStadium(stadium);
+        playSoccer.setTeamHome(teamHome);
+        playSoccer.setTeamAway(teamAway);
+        playSoccer.setBall(ball);
+        playSoccer.setTeamHomeGoals(teamHomeGoal);
+        playSoccer.setTeamAwayGoals(teamAwayGoal);
+        playSoccer.setTeamHomePoints(teamHomePoint);
+        playSoccer.setTeamAwayPoints(teamAwayPoint);
+        new PlaySoccerService().saveOrUpdate(playSoccer);
+        teamHome.setScore(teamHome.getScore() + teamHomePoint);
+        teamAway.setScore(teamAway.getScore() + teamAwayPoint);
+        new TeamService().saveOrUpdate(teamHome);
+        new TeamService().saveOrUpdate(teamAway);
     }
 
     @Override
